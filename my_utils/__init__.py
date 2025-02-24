228b39e0fb198f1934866150a5a89aa1870b2930
Omar <darkev777@gmail.com>
2025-02-24 05:56:41 +0400

    inital commit w/ tag

--- my_utils/__init__.py
+++ my_utils/__init__.py
@@ -5,3 +5,11 @@ from .predictors import select_important_features, ExogArima, predict_churn
 from .dataframes import smart_drop, NumericalScaler, CategoricalEncoder, optimize_dtypes
 from .letsplot_pane import LetsPlotPane
 from .shap_calculator import get_shap_values
+
+# Defines the public API of the package, limiting what gets imported via "from my_utils import *" to elements in "__all__"
+__all__ = ("clean_columns",
+           "select_important_features", "ExogArima", "predict_churn",
+           'smart_drop', 'NumericalScaler', 'CategoricalEncoder', 'optimize_dtypes',
+           "LetsPlotPane",
+           "get_shap_values",
+           )
