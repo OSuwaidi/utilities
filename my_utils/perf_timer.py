33763e6cbdef2ddd0b75ca9aa1cae4b47d69dfd1
Omar <darkev777@gmail.com>
2025-03-07 23:40:37 +0400

    reformat

--- my_utils/perf_timer.py
+++ my_utils/perf_timer.py
@@ -1,24 +1,21 @@
 import timeit
-import os
+import numpy as np
 
 # Approach 1
-def approach_1(cmd):
-    for path in filter(os.path.isdir, os.environ["PATH"].split(os.pathsep)):  # "os.pathsep" is ";" for Windows and ":" for Unix)
-        if os.path.isfile((full_path := os.path.join(path, cmd))) and os.access(full_path, os.X_OK):  # if file exists and is an executable...
-            return full_path
+def approach_1(lengths):
+    [np.random.randn(l) + 1 for l in lengths]
 
 
 # Approach 2
-def approach_2(cmd):
-    for path in os.environ["PATH"].split(os.pathsep):  # "os.pathsep" is ";" for Windows and ":" for Unix)
-        if os.path.isdir(path) and os.path.isfile((full_path := os.path.join(path, cmd))) and os.access(full_path, os.X_OK):  # if file exists and is an executable...
-            return full_path
+def approach_2(lengths):
+    [np.random.randn(l) + np.ones(l) for l in lengths]
 
 # Input data
+lengthsies = [10*i for i in range(1, 501)]
 
 # Measure time
-time_approach_1 = timeit.timeit(lambda: approach_1("ls"), number=1000)
-time_approach_2 = timeit.timeit(lambda: approach_2("ls"), number=1000)
+time_approach_1 = timeit.timeit(lambda: approach_1(lengthsies), number=100)
+time_approach_2 = timeit.timeit(lambda: approach_2(lengthsies), number=100)
 
 
 print(f"Approach 1 took {time_approach_1:.4f} seconds")
