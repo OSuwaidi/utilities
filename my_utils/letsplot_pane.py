353585eee860c0801a43f4dd4c8adb83bf3f30db
Omar <omar.ali.alsuwaidi@gmail.com>
2025-04-09 11:42:41 +0400

    adapt to new lets-plot version breaking changes

--- my_utils/letsplot_pane.py
+++ my_utils/letsplot_pane.py
@@ -1,41 +1,47 @@
 import param
 from panel.reactive import ReactiveHTML
 
-from lets_plot import __version__ as _lets_plot_version
+import base64
 from lets_plot.plot.core import PlotSpec
-from lets_plot._type_utils import standardize_dict
+from lets_plot._kbridge import (
+    _generate_static_configure_html,
+    _generate_display_html_for_raw_spec
+)
 
 import polars as pl
+import pandas as pd
 
 
 class LetsPlotPane(ReactiveHTML):
-    object: PlotSpec = param.ClassSelector(class_=PlotSpec, precedence=-1)
+    # Param objects are initialized and assigned upon instantiation
+    plot_object = param.ClassSelector(class_=PlotSpec, precedence=-1)  # default = None
+    sizing_options = param.Dict(default={"width_mode": "fit", "height_mode": "fit"})
+    plot_uri = param.String()  # default = ""
 
-    _plot_spec_as_dict: dict = param.Dict()
+    config_html: str = _generate_static_configure_html()  # load necessary JS/CSS boilerplate
+    _template = """
+    <iframe id="pn-container" src="data:text/html;base64,${plot_uri}" style="height:100%; width:100%; border: none;"></iframe>
+    """  # HTML template that gets rendered and declares how sublass' parameters are linked to HTML
 
-    _template = '<div id="pn-container" style="height:100%;width:100%"></div>'
-
-    __javascript__ = [f"https://cdn.jsdelivr.net/gh/JetBrains/lets-plot@v{_lets_plot_version}/js-package/distr/lets-plot.min.js"]
-
-    @param.depends("object", watch=True, on_init=True)
+    @param.depends("plot_object", watch=True, on_init=True)
     def _update_config(self) -> None:
-        if not self.object:
-            self._plot_spec_as_dict = {}
-        else:
-            spec: dict = self.object.as_dict()
-            if "data" in spec and isinstance(spec["data"], pl.DataFrame):
-                spec["data"] = spec["data"].to_dict(as_series=False)
+        if not self.plot_object:
+            self.plot_uri = ""
+            return
 
-            self._plot_spec_as_dict = standardize_dict(spec)
+        spec: dict = self.plot_object.as_dict()
+        if (data := spec.get("data")) is not None:
+            if isinstance(data, pl.DataFrame):
+                spec["data"] = data.to_dict(as_series=False)
+            elif isinstance(data, pd.DataFrame):
+                spec["data"] = data.to_dict(orient="list")
 
-    _scripts = {
-        "render": "state.height=-10",
-        "after_layout": "self._plot_spec_as_dict()",
-        "_plot_spec_as_dict": """
-var height=pn_container.clientHeight
-if (state.height-5<=height & height<=state.height+5){height=state.height}
-state.height=height
-pn_container.innerHTML=""
-LetsPlot.buildPlotFromRawSpecs(data._plot_spec_as_dict, pn_container.clientWidth-5, height, pn_container);
-""",
-    }
+        display_html = _generate_display_html_for_raw_spec(spec, sizing_options=self.sizing_options, responsive=True)
+        plot_html = f"""
+        {LetsPlotPane.config_html}
+        {display_html}
+        """
+
+        html_bytes = plot_html.encode("utf-8")
+        html_b64 = base64.b64encode(html_bytes).decode("utf-8")
+        self.plot_uri = html_b64
