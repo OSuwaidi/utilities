39d81863c1505866e4f12b78921400bdf946a9b4
Omar <omar.ali.alsuwaidi@gmail.com>
2025-04-13 01:06:42 +0400

    improve plot size and precompute config html

--- my_utils/letsplot_pane.py
+++ my_utils/letsplot_pane.py
@@ -11,17 +11,22 @@ from lets_plot._kbridge import (
 import polars as pl
 import pandas as pd
 
+_config_html: bytes = _generate_static_configure_html().encode("utf-8")  # load necessary JS/CSS boilerplate
+
 
 class LetsPlotPane(ReactiveHTML):
-    # Param objects are initialized and assigned upon instantiation
+    # Param objects are initialized and bound (to instances) upon object instantiation
     plot_object = param.ClassSelector(class_=PlotSpec, precedence=-1)  # default = None
     sizing_options = param.Dict(default={"width_mode": "fit", "height_mode": "fit"})
     plot_uri = param.String()  # default = ""
 
-    config_html: str = _generate_static_configure_html()  # load necessary JS/CSS boilerplate
     _template = """
-    <iframe id="pn-container" src="data:text/html;base64,${plot_uri}" style="height:100%; width:100%; border: none;"></iframe>
-    """  # HTML template that gets rendered and declares how sublass' parameters are linked to HTML
+    <iframe
+        id="pn-container"
+        src="data:text/html;base64,${plot_uri}"
+        style="height:1.8vh; width:42vw; border: none;">
+    </iframe>
+    """  # HTML template that gets rendered and declares how the sublass' parameters are linked to HTML
 
     @param.depends("plot_object", watch=True, on_init=True)
     def _update_config(self) -> None:
@@ -36,12 +41,7 @@ class LetsPlotPane(ReactiveHTML):
             elif isinstance(data, pd.DataFrame):
                 spec["data"] = data.to_dict(orient="list")
 
-        display_html = _generate_display_html_for_raw_spec(spec, sizing_options=self.sizing_options, responsive=True)
-        plot_html = f"""
-        {LetsPlotPane.config_html}
-        {display_html}
-        """
+        display_html: str = _generate_display_html_for_raw_spec(spec, sizing_options=self.sizing_options, responsive=True)
 
-        html_bytes = plot_html.encode("utf-8")
-        html_b64 = base64.b64encode(html_bytes).decode("utf-8")
-        self.plot_uri = html_b64
+        plot_html_bytes = _config_html + display_html.encode("utf-8")
+        self.plot_uri = base64.b64encode(plot_html_bytes).decode("utf-8")
